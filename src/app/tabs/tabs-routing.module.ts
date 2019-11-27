5267852ecf104b89dea384c946c9a19b4d25e882
Tasos Adamou <tasos@Tasoss-MacBook-Pro.local>
2019-11-27 13:01:38 +1100

    create home page, tabs routing, tabs html

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -2,53 +2,38 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
-const routes: Routes = [
+const routes: Routes = 
+[
   {
     path: 'tabs',
     component: TabsPage,
-    children: [
+
+    children: 
+    [
       {
-        path: 'tab1',
+        path: 'home-page',
         children: [
           {
-            path: '',
-            loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+            path:'',
+            loadChildren: () => import('../home-page/home-page.module').then( m => m.HomePagePageModule )
           }
         ]
       },
-      {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-          }
-        ]
-      },
+
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home-page',
         pathMatch: 'full'
       }
     ]
   },
+
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home-page',
     pathMatch: 'full'
   }
+
 ];
 
 @NgModule({
