d642f7a058b327639c99b5c7bfee5925587b88b3
Tasos Adamou <tasos@Tasoss-MacBook-Pro.local>
2019-11-28 17:18:51 +1100

    commented out the alert on home-page

--- src/app/home-page/home-page.page.ts
+++ src/app/home-page/home-page.page.ts
@@ -60,23 +60,23 @@ export class HomePagePage implements OnInit
 
 
 
-  async buttonAlertOnClick()
-  {
-    let task: Task =
-    {
-      name:     this.formGroupTask.get('typedTask').value,
-      dueDate:  new Date(),
-      category: this.formGroupTask.get('typedCat').value
-    }
+  // async buttonAlertOnClick()
+  // {
+  //   let task: Task =
+  //   {
+  //     name:     this.formGroupTask.get('typedTask').value,
+  //     dueDate:  new Date(),
+  //     category: this.formGroupTask.get('typedCat').value
+  //   }
 
-    this.dateStr = new Intl.DateTimeFormat('en-US', {weekday:'long'}).format(task.dueDate)  + ', ' 
-                    + task.dueDate.getDate().toString()                                     + '-'
-                    + new Intl.DateTimeFormat('en-US', {month:'short'}).format(task.dueDate) + '-'
-                    + task.dueDate.getFullYear().toString();
+  //   this.dateStr = new Intl.DateTimeFormat('en-US', {weekday:'long'}).format(task.dueDate)  + ', ' 
+  //                   + task.dueDate.getDate().toString()                                     + '-'
+  //                   + new Intl.DateTimeFormat('en-US', {month:'short'}).format(task.dueDate) + '-'
+  //                   + task.dueDate.getFullYear().toString();
                     
-    const alert = this.alertController.create({ header:task.name, subHeader:this.dateStr, message:task.category, buttons:['OK'] });
-    (await alert).present();
-  }
+  //   const alert = this.alertController.create({ header:task.name, subHeader:this.dateStr, message:task.category, buttons:['OK'] });
+  //   (await alert).present();
+  // }
 
 
 }
